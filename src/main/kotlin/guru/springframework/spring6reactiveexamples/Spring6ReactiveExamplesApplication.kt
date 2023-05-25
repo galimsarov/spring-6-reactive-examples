0be74fddeb0afb2da509bee73408bad3bd0b1fca
Евгений Галимсаров <address@email.com>
2023-05-25 09:54:46 +0500

    Initial commit

---
+++ src/main/kotlin/guru/springframework/spring6reactiveexamples/Spring6ReactiveExamplesApplication.kt
@@ -0,0 +1,11 @@
+package guru.springframework.spring6reactiveexamples
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Spring6ReactiveExamplesApplication
+
+fun main(args: Array<String>) {
+	runApplication<Spring6ReactiveExamplesApplication>(*args)
+}
